c07dd246ccde2282179525e56307941465890ee7
Krishna <kp602552@gmail.com>
2026-06-11 20:10:49 +0530

    feat: initial Spring Boot project setup

---
+++ src/test/java/com/cryptosim/CryptosimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptosim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptosimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
